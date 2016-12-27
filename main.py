d6caf593ef70abe7fd7194f27914d9af5cd13997
Bobby Walker <gwaland@gmail.com>
2016-12-26 19:00:30 -0600

    added config.py.example and adjusted so this only triggers on waking from deep sleep not just any wake up.

--- main.py
+++ main.py
@@ -4,9 +4,7 @@ import machine
 
 #connect to wifi
 
-wemo_ip = '10.3.0.9'
-plug_name = 'piu-plug'
-
+#If you have an LED linked to GPIO0 then this will turn it on. 
 led = machine.Pin(0, machine.Pin.OUT)
 led.high()
 
@@ -15,14 +13,17 @@ do_connect()
 
 if machine.reset_cause() == machine.DEEPSLEEP_RESET:
     print('woke from a deep sleep')
+    print('Running command!')
+    trigger_button()
 else:
     print('power on or hard reset')
+    #comment this out to drop to repl for debuging.
+    machine.deepsleep()
 
-
-url = 'http://%s:5000/api/device/%s' % (wemo_ip, plug_name)
-headers = {'Content-Type' : 'text/json; charset=utf-8'}
-data = '{state: toggle}'
-resp = urequests.post(url, data=data, headers=headers)
-print(resp.json())
-
-#machine.deepsleep()
+def trigger_button():
+    url = 'http://%s:5000/api/device/%s' % (wemo_ip, plug_name)
+    headers = {'Content-Type' : 'text/json; charset=utf-8'}
+	data = '{state: toggle}'
+	resp = urequests.post(url, data=data, headers=headers)
+	print(resp.json())
+	machine.deepsleep()
